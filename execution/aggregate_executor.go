d568e34188f037b7a98bf8756fad207f02fca057
chase06harmon <chase1harmon@gmail.com>
2026-03-18 00:51:54 -0400

    aggregator

--- execution/aggregate_executor.go
+++ execution/aggregate_executor.go
@@ -1,40 +1,191 @@
 package execution
 
 import (
+	"mit.edu/dsg/godb/common"
 	"mit.edu/dsg/godb/planner"
 	"mit.edu/dsg/godb/storage"
 )
 
 // AggregateExecutor implements hash-based aggregation.
 type AggregateExecutor struct {
-	planNode      *planner.SortNode
+	planNode      *planner.AggregateNode
 	childExecutor Executor
+	hashTable     *ExecutionHashTable[[]common.Value]
+	iterChan      chan storage.Tuple
+	curTuple      storage.Tuple
 }
 
 func NewAggregateExecutor(plan *planner.AggregateNode, child Executor) *AggregateExecutor {
-	panic("unimplemented")
+	numGroupByKeys := len(plan.GroupByClause)
+	var hashTable *ExecutionHashTable[[]common.Value]
+
+	if numGroupByKeys > 0 {
+		groupByKeyTypes := make([]common.Type, len(plan.GroupByClause))
+		for i, expr := range plan.GroupByClause {
+			groupByKeyTypes[i] = expr.OutputType()
+		}
+		hashTable = NewExecutionHashTable[[]common.Value](storage.NewRawTupleDesc(groupByKeyTypes))
+	}
+
+	aggExec := AggregateExecutor{
+		planNode:      plan,
+		childExecutor: child,
+		hashTable:     hashTable,
+	}
+
+	aggExec.iterChan = make(chan storage.Tuple)
+
+	return &aggExec
 }
 
 func (e *AggregateExecutor) PlanNode() planner.PlanNode {
-	panic("unimplemented")
+	return e.planNode
 }
 
 func (e *AggregateExecutor) Init(ctx *ExecutorContext) error {
-	panic("unimplemented")
+	e.childExecutor.Init(ctx)
+
+	if len(e.planNode.GroupByClause) > 0 {
+		exists := e.childExecutor.Next()
+		if !exists {
+			close(e.iterChan)
+			return nil
+		}
+		for ; exists; exists = e.childExecutor.Next() {
+			curTup := e.childExecutor.Current()
+			keyVals := make([]common.Value, len(e.planNode.GroupByClause))
+
+			for i, groupByExpr := range e.planNode.GroupByClause {
+				keyVals[i] = groupByExpr.Eval(curTup)
+			}
+			keyTup := storage.FromValues(keyVals...)
+			curAgg, exists := e.hashTable.Get(keyTup)
+			if exists {
+				for i, aggExpr := range e.planNode.AggClauses {
+					curAggVal := curAgg[i]
+					newVal := aggExpr.Expr.Eval(curTup)
+					curAgg[i] = computeNewAggValue(curAggVal, newVal, aggExpr.Type)
+				}
+			} else {
+				aggVals := make([]common.Value, len(e.planNode.AggClauses))
+				for i, aggExpr := range e.planNode.AggClauses {
+					var curAggVal common.Value
+					if aggExpr.Type == planner.AggCount {
+						curAggVal = common.NewIntValue(0)
+					} else if aggExpr.Expr.OutputType() == common.IntType {
+						curAggVal = common.NewNullInt()
+					} else {
+						curAggVal = common.NewNullString()
+					}
+					newVal := aggExpr.Expr.Eval(curTup)
+					aggVals[i] = computeNewAggValue(curAggVal, newVal, aggExpr.Type)
+				}
+				e.hashTable.Insert(keyTup, aggVals)
+			}
+		}
+
+		go func() {
+			e.hashTable.Iterate(func(key storage.Tuple, value []common.Value) {
+				e.iterChan <- key.Extend(value)
+			})
+			close(e.iterChan)
+		}()
+
+		return nil
+	} else {
+		aggVals := make([]common.Value, len(e.planNode.AggClauses))
+		for i, aggExpr := range e.planNode.AggClauses {
+			var curAggVal common.Value
+			if aggExpr.Type == planner.AggCount {
+				curAggVal = common.NewIntValue(0)
+			} else if aggExpr.Expr.OutputType() == common.IntType {
+				curAggVal = common.NewNullInt()
+			} else {
+				curAggVal = common.NewNullString()
+			}
+			aggVals[i] = curAggVal
+		}
+		exists := e.childExecutor.Next()
+		if !exists {
+			close(e.iterChan)
+			return nil
+		}
+
+		for ; exists; exists = e.childExecutor.Next() {
+			curTup := e.childExecutor.Current()
+			for i, aggExpr := range e.planNode.AggClauses {
+				curAggVal := aggVals[i]
+				newVal := aggExpr.Expr.Eval(curTup)
+				aggVals[i] = computeNewAggValue(curAggVal, newVal, aggExpr.Type).Copy()
+			}
+		}
+
+		go func() {
+			e.iterChan <- storage.FromValues(aggVals...)
+			close(e.iterChan)
+		}()
+
+		return nil
+	}
+
+}
+
+func computeNewAggValue(curAggVal common.Value, newVal common.Value, aggType planner.AggregatorType) common.Value {
+	// fmt.Println(curAggVal, ", ", newVal)
+	if newVal.IsNull() {
+		return curAggVal
+	}
+
+	if curAggVal.IsNull() {
+		switch aggType {
+		case planner.AggCount:
+			return common.NewIntValue(1)
+		default:
+			return newVal
+		}
+
+	} else { // both are not null
+		switch aggType {
+		case planner.AggCount:
+			return curAggVal.Increment()
+		case planner.AggSum:
+			return common.NewIntValue(curAggVal.IntValue() + newVal.IntValue())
+		case planner.AggMin:
+			if curAggVal.Compare(newVal) == 1 { // curAggVal > newVal
+				return newVal
+			} else {
+				return curAggVal
+			}
+		case planner.AggMax:
+			if curAggVal.Compare(newVal) == -1 { // curAggVal < newVal
+				return newVal
+			} else {
+				return curAggVal
+			}
+
+		default:
+			return curAggVal
+		}
+	}
 }
 
 func (e *AggregateExecutor) Next() bool {
-	panic("unimplemented")
+	tup, ok := <-e.iterChan
+	if !ok {
+		return false
+	}
+	e.curTuple = tup
+	return true
 }
 
 func (e *AggregateExecutor) Current() storage.Tuple {
-	panic("unimplemented")
+	return e.curTuple
 }
 
 func (e *AggregateExecutor) Error() error {
-	panic("unimplemented")
+	return e.childExecutor.Error()
 }
 
 func (e *AggregateExecutor) Close() error {
-	panic("unimplemented")
+	return e.childExecutor.Close()
 }
