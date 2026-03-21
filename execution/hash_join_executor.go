2c269159e5c2bf4fc447c95c134d8b42259e546c
chase06harmon <chase1harmon@gmail.com>
2026-03-20 23:01:55 -0400

    pt 4

--- execution/hash_join_executor.go
+++ execution/hash_join_executor.go
@@ -1,6 +1,9 @@
 package execution
 
 import (
+	"fmt"
+
+	"mit.edu/dsg/godb/common"
 	"mit.edu/dsg/godb/planner"
 	"mit.edu/dsg/godb/storage"
 )
@@ -9,34 +12,196 @@ import (
 // It builds a hash table from the left child and probes it with the right child.
 // It only supports Equi-Joins.
 type HashJoinExecutor struct {
-	// Fill me in!
+	planNode           *planner.HashJoinNode
+	leftChildExecutor  Executor
+	rightChildExecutor Executor
+	hashTable          *ExecutionHashTable[*[]storage.Tuple]
+	curRightTuple      storage.Tuple
+	curNumOutput       int
+	combBuf            []byte
+	combDesc           *storage.RawTupleDesc
 }
 
 // NewHashJoinExecutor creates a new HashJoinExecutor.
 func NewHashJoinExecutor(plan *planner.HashJoinNode, left Executor, right Executor) *HashJoinExecutor {
-	panic("unimplemented")
+	combTypes := append(left.PlanNode().OutputSchema(), right.PlanNode().OutputSchema()...)
+	combDesc := storage.NewRawTupleDesc(combTypes)
+
+	combBuf := make([]byte, combDesc.BytesPerTuple())
+
+	hashJoinExec := HashJoinExecutor{
+		planNode:           plan,
+		leftChildExecutor:  left,
+		rightChildExecutor: right,
+		curNumOutput:       -1,
+		combBuf:            combBuf,
+		combDesc:           combDesc,
+	}
+	return &hashJoinExec
 }
 
 func (e *HashJoinExecutor) PlanNode() planner.PlanNode {
-	panic("unimplemented")
+	return e.planNode
+}
+
+// NOTE: Assumes that must have at least a predicate on each side! Otherwise, equivalent to a filter
+
+type InvalidHashJoinKeysError struct{}
+
+func (InvalidHashJoinKeysError) Error() string {
+	return "Insufficient keys for a hash join"
 }
 
 func (e *HashJoinExecutor) Init(ctx *ExecutorContext) error {
-	panic("unimplemented")
+	err := e.leftChildExecutor.Init(ctx)
+	if err != nil {
+		return err
+	}
+	err = e.rightChildExecutor.Init(ctx)
+	if err != nil {
+		return err
+	}
+
+	if len(e.planNode.LeftKeys) == 0 || len(e.planNode.RightKeys) == 0 {
+		return InvalidHashJoinKeysError{}
+	}
+
+	var hashTable *ExecutionHashTable[*[]storage.Tuple]
+
+	joinKeyTypes := make([]common.Type, len(e.planNode.LeftKeys))
+	for i, expr := range e.planNode.LeftKeys {
+		joinKeyTypes[i] = expr.OutputType()
+	}
+	hashTable = NewExecutionHashTable[*[]storage.Tuple](storage.NewRawTupleDesc(joinKeyTypes))
+
+	e.hashTable = hashTable
+
+	for exists := e.leftChildExecutor.Next(); exists; exists = e.leftChildExecutor.Next() {
+		curTup := e.leftChildExecutor.Current()
+		keyVals := make([]common.Value, len(e.planNode.LeftKeys))
+		nullKey := false
+
+		for i, keyExpr := range e.planNode.LeftKeys {
+			keyVal := keyExpr.Eval(curTup)
+			if keyVal.IsNull() {
+				nullKey = true
+				break
+			}
+			keyVals[i] = keyVal
+		}
+
+		if nullKey {
+			continue
+		}
+
+		keyTup := storage.FromValues(keyVals...)
+		curTupsPtr, exists := e.hashTable.Get(keyTup)
+		curTupCopy := curTup.DeepCopy(storage.NewRawTupleDesc(e.planNode.Left.OutputSchema()))
+		if exists {
+			*curTupsPtr = append(*curTupsPtr, curTupCopy)
+		} else {
+			curTups := []storage.Tuple{curTupCopy}
+			e.hashTable.Insert(keyTup, &curTups)
+		}
+	}
+
+	e.curNumOutput = -1
+
+	return nil
+}
+
+func printTupleVal(tup storage.Tuple) {
+	fmt.Printf("[")
+	for i := 0; i < tup.NumColumns(); i++ {
+		switch x := tup.GetValue(i); x.Type() {
+		case common.IntType:
+			fmt.Printf("%d\t", x.IntValue())
+		case common.StringType:
+			fmt.Printf("%s\t", x.StringValue())
+		default:
+			fmt.Printf("NULL\t")
+		}
+
+	}
+
+	fmt.Println("]")
 }
 
 func (e *HashJoinExecutor) Next() bool {
-	panic("unimplemented")
+	if e.curNumOutput < 0 {
+		if !e.rightChildExecutor.Next() {
+			return false
+		}
+		e.curNumOutput = 0
+	}
+
+	for {
+		curTup := e.rightChildExecutor.Current()
+		keyVals := make([]common.Value, len(e.planNode.RightKeys))
+		nullKey := false
+
+		for i, keyExpr := range e.planNode.RightKeys {
+			keyVal := keyExpr.Eval(curTup)
+			if keyVal.IsNull() {
+				nullKey = true
+				break
+			}
+			keyVals[i] = keyVal
+		}
+
+		if nullKey {
+			if !e.rightChildExecutor.Next() {
+				return false
+			}
+			e.curNumOutput = 0
+			continue
+		}
+
+		keyTup := storage.FromValues(keyVals...)
+		matches, exists := e.hashTable.Get(keyTup)
+
+		if exists && e.curNumOutput < len(*matches) {
+			leftTup := (*matches)[e.curNumOutput]
+			storage.MergeTuples(e.combBuf, e.combDesc, leftTup, curTup)
+			e.curNumOutput++
+			return true
+		} else {
+			if !e.rightChildExecutor.Next() {
+				return false
+			}
+			e.curNumOutput = 0
+		}
+	}
 }
 
 func (e *HashJoinExecutor) Current() storage.Tuple {
-	panic("unimplemented")
+	return storage.FromRawTuple(e.combBuf, e.combDesc, common.RecordID{})
 }
 
 func (e *HashJoinExecutor) Error() error {
-	panic("unimplemented")
+	rightErr := e.rightChildExecutor.Error()
+	leftErr := e.leftChildExecutor.Error()
+
+	if rightErr != nil {
+		return rightErr
+	}
+
+	if leftErr != nil {
+		return leftErr
+	}
+
+	return nil
 }
 
 func (e *HashJoinExecutor) Close() error {
-	panic("unimplemented")
+	err := e.rightChildExecutor.Close()
+	if err != nil {
+		return err
+	}
+	err = e.leftChildExecutor.Close()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- execution/topn_executor.go
+++ execution/topn_executor.go
@@ -1,6 +1,9 @@
 package execution
 
 import (
+	"container/heap"
+	"sort"
+
 	"mit.edu/dsg/godb/planner"
 	"mit.edu/dsg/godb/storage"
 )
@@ -8,34 +11,128 @@ import (
 // TopNExecutor optimizes "ORDER BY ... LIMIT N" queries.
 //
 // This should allow an optimized implementation that avoids sorting ALL tuples (O(M log M)).
+
+type TupleHeap struct {
+	items    []storage.Tuple
+	orderBys []planner.OrderByClause
+}
+
+func (h *TupleHeap) Len() int {
+	return len(h.items)
+}
+
+func (h *TupleHeap) Less(i, j int) bool {
+	leftTuple := h.items[i]
+	rightTuple := h.items[j]
+
+	for _, orderBy := range h.orderBys {
+		leftVal := orderBy.Expr.Eval(leftTuple)
+		rightVal := orderBy.Expr.Eval(rightTuple)
+
+		comparison := leftVal.Compare(rightVal)
+		if comparison == 0 {
+			continue
+		}
+
+		if orderBy.Direction == planner.SortOrderDescending {
+			return comparison < 0
+		}
+		return comparison > 0
+	}
+
+	return false
+}
+
+func (h *TupleHeap) Swap(i, j int) {
+	h.items[i], h.items[j] = h.items[j], h.items[i]
+}
+
+func (h *TupleHeap) Push(x interface{}) {
+	h.items = append(h.items, x.(storage.Tuple))
+}
+
+func (h *TupleHeap) Pop() interface{} {
+	old := h.items
+	n := len(old)
+	tuple := old[n-1]
+	h.items = old[:n-1]
+	return tuple
+}
+
 type TopNExecutor struct {
-	// Fill me in!
+	planNode      *planner.TopNNode
+	childExecutor Executor
+	heap          *TupleHeap
+	numOutput     int
 }
 
 func NewTopNExecutor(plan *planner.TopNNode, child Executor) *TopNExecutor {
-	panic("unimplemented")
+	topNExecutor := TopNExecutor{
+		planNode:      plan,
+		childExecutor: child,
+		heap: &TupleHeap{
+			items:    make([]storage.Tuple, 0),
+			orderBys: plan.OrderBy,
+		},
+	}
+	return &topNExecutor
 }
 
 func (e *TopNExecutor) PlanNode() planner.PlanNode {
-	panic("unimplemented")
+	return e.planNode
 }
 
 func (e *TopNExecutor) Init(ctx *ExecutorContext) error {
-	panic("unimplemented")
+	err := e.childExecutor.Init(ctx)
+	if err != nil {
+		return err
+	}
+	heap.Init(e.heap)
+
+	for exists := e.childExecutor.Next(); exists; exists = e.childExecutor.Next() {
+		curTup := e.childExecutor.Current()
+		heap.Push(e.heap, curTup.DeepCopy(storage.NewRawTupleDesc(e.childExecutor.PlanNode().OutputSchema())))
+		if e.heap.Len() > e.planNode.Limit {
+			heap.Pop(e.heap)
+		}
+	}
+
+	sort.Slice(e.heap.items, func(i, j int) bool {
+		a, b := e.heap.items[i], e.heap.items[j]
+		for _, ob := range e.planNode.OrderBy {
+			cmp := ob.Expr.Eval(a).Compare(ob.Expr.Eval(b))
+			if cmp == 0 {
+				continue
+			}
+			if ob.Direction == planner.SortOrderAscending {
+				return cmp < 0
+			}
+			return cmp > 0
+		}
+		return false
+	})
+
+	e.numOutput = 0
+
+	return nil
 }
 
 func (e *TopNExecutor) Next() bool {
-	panic("unimplemented")
+	if e.numOutput < e.planNode.Limit && e.numOutput < len(e.heap.items) {
+		e.numOutput++
+		return true
+	}
+	return false
 }
 
 func (e *TopNExecutor) Current() storage.Tuple {
-	panic("unimplemented")
+	return e.heap.items[e.numOutput-1]
 }
 
 func (e *TopNExecutor) Error() error {
-	panic("unimplemented")
+	return e.childExecutor.Error()
 }
 
 func (e *TopNExecutor) Close() error {
-	panic("unimplemented")
+	return e.childExecutor.Close()
 }
